a0ba6df0213f3a6c167c5a3581ab24d22f448373
Eugene Naumov <min.verkligheten@gmail.com>
2023-09-05 22:02:49 +0300

    Update prompts_controller.rb

--- app/controllers/prompts_controller.rb
+++ app/controllers/prompts_controller.rb
@@ -1,18 +1,19 @@
 class PromptsController < ApplicationController
-  def index
-
-  end
+  def index; end
 
   def search
-    @prompts =
-      Prompt.search(permitted_params[:q], match: :text_middle)
-            .records
-            .records
+    @prompts = Prompt.search(permitted_params[:q], match: :text_middle).limit(5)
+
+    render turbo_stream: turbo_stream.update(
+      'prompts-results',
+      partial: 'prompts/prompts',
+      locals: { prompts: @prompts }
+    )
   end
 
   private
 
   def permitted_params
-    params.permit(:q)
+    params.require(:prompt).permit(:q)
   end
 end
